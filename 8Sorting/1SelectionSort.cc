ea555e123e7e32f669bfc576f84e19f8c135f2cc
Adi <helloadig@gmail.com>
2023-02-11 13:15:18 +0530

    selection sort : algo, flowchart, logic.

---
+++ 8Sorting/1SelectionSort.cc
@@ -0,0 +1,35 @@
+// arr = 4, 3, 2, 1
+// Time complexity - O(n^2)
+// Space Complexity - O(1)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> arr){
+
+    int n = arr.size();
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if(arr[j]<arr[i])
+                swap(arr[i], arr[j]);
+        }
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout<<" "<<arr[i];
+    }
+}
+
+int main()
+{    
+    // vector<int> v1 = {4, 3, 2, 1};
+    // vector<int> v1 = {4, 3, 2, 1, 10, 9, 8};
+    vector<int> v1 = {5, 3, 6, 9};
+    cout<<"Selection Sort Array is :";
+    selectionSort(v1);
+    return 0;
+}
